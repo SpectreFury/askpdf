883a99581813b1e927d71b5898289ec0c4f8b134
Ayush Soni <soni.ayush.2212@gmail.com>
2026-09-20 13:45:41 +0530

    feat: update globals to match our styles and add navigation bar

---
+++ web/app/_components/NavBar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <div className="border">
+      <nav className="w-full flex items-center justify-between container mx-auto py-2">
+        <div className="flex items-center gap-2">
+          <div className="font-display font-medium uppercase bg-primary px-2 rounded text-white">
+            L
+          </div>
+          <span className="font-bold">AskPDF</span>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Link href="/login" className="text-sm font-medium">
+            Sign in
+          </Link>
+          <Link
+            href="/signup"
+            className="text-sm bg-primary text-card primary p-2 rounded"
+          >
+            Start Free Trial
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
