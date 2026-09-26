0d91587b3443a6e2ad2b86aac36b2d8e47d0ca7a
Ayush Soni <soni.ayush.2212@gmail.com>
2026-09-26 17:32:53 +0530

    feat: add session model

--- server/src/db/models/auth_models.py
+++ server/src/db/models/auth_models.py
@@ -1,8 +1,8 @@
 import datetime
 import uuid
 
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Column, String, Text, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Column, ForeignKey, String, Text, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from ..db import Base
 
@@ -15,10 +15,26 @@ class User(Base):
     )
     first_name: Mapped[str] = mapped_column(nullable=False)
     last_name: Mapped[str] = mapped_column(nullable=False)
-    email: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=True)
     is_google_account: Mapped[bool] = mapped_column(nullable=True)
     created_at: Mapped[datetime.datetime] = mapped_column(
         DateTime(timezone=True),
         default=lambda: datetime.datetime.now(datetime.timezone.utc),
     )
+
+    sessions: Mapped[list["Session"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    title: Mapped[str] = mapped_column(nullable=False)
+    document_id: Mapped[str] = mapped_column(nullable=True)
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="sessions")
